fbc68982918255c44e7763149ecdfe77bd32309f
luoxg <1833101466@qq.com>
2023-04-20 23:40:40 +0800

    first

---
+++ encode/4-1  sessionhello.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 28 06:09:31 2017
+
+@author: 代码医生 qq群：40016981，公众号：xiangyuejiqiren
+@blog：http://blog.csdn.net/lijin6249
+"""
+import tensorflow.compat.v1 as tf
+tf.disable_eager_execution()
+hello = tf.constant('Hello, TensorFlow!')  #定义一个常量
+sess = tf.Session()                             #建立一个session
+print (sess.run(hello))                        #通过session里面的run来运行结果
+sess.close()                                     #关闭session
+
+
+
